3b4b333d52eba5a1db2081da0d42ce31e3cf8da0
Mattiasslzr <mattias2317@icloud.com>
2023-10-03 20:39:22 -0300

    ult
    ult

--- HotelIdealG81/src/Main/HotelIdealG81.java
+++ HotelIdealG81/src/Main/HotelIdealG81.java
@@ -1,12 +1,17 @@
-
 package Main;
 
+import AccesoADatos.HuespedData;
+import Entidades.Huesped;
 
 class HotelIdealG81 {
 
 
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+       HuespedData hd= new HuespedData();
+       Huesped h= new Huesped();
+       
+       Huesped D = new Huesped ("juan", "Dominguez", 40345523, "Av.Velez Sarfield", "juandominguez@gmail.com", 351478965);
+             HuespedData hue = new HuespedData();
+  }
     
 }
