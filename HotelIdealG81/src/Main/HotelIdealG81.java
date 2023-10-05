792b854d9e36258ea19b482e62b0678baed92a85
FranMacAu <119713089+FranMacAu@users.noreply.github.com>
2023-10-04 22:58:23 -0300

    ABM HuespedData
    Hay que probar los métodos

--- HotelIdealG81/src/Main/HotelIdealG81.java
+++ HotelIdealG81/src/Main/HotelIdealG81.java
@@ -10,9 +10,13 @@ class HotelIdealG81 {
        HuespedData hd= new HuespedData();
        Huesped h= new Huesped();
        
-       Huesped D = new Huesped ("juan", "Dominguez", 40345523, "Av.Velez Sarfield", "juandominguez@gmail.com", 351478965);
+       Huesped D = new Huesped ("Maria", "de las Calores", 00000011, "Colon 1232", "originalmariacalores@hotmail.com", 353478962, true);
        HuespedData hue = new HuespedData();
-       hd.GuardarHuesped(D);
+       
+       //hd.GuardarHuesped(D);
+       //hd.modificarHuesped(D);
+       //hd.eliminarHuesped(8);
+       //System.out.println(D.getIdHuesped());
   }
     
 }
